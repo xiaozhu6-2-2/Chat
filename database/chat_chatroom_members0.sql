55addcfbe1b4bae6ecb733d967936baa3b69bbfe
2huzhu <3206451516@qq.com>
2025-07-10 21:10:10 +0800

    0.3.2 database

---
+++ database/chat_chatroom_members0.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: chat
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chatroom_members`
+--
+
+DROP TABLE IF EXISTS `chatroom_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chatroom_members` (
+  `chatroom_id` bigint NOT NULL,
+  `account` varchar(45) NOT NULL,
+  `joined_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_seen` timestamp NULL DEFAULT NULL,
+  `is_online` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`chatroom_id`,`account`),
+  KEY `fk_member_account_idx` (`account`),
+  CONSTRAINT `fk_member_account` FOREIGN KEY (`account`) REFERENCES `user_info` (`account`) ON DELETE CASCADE,
+  CONSTRAINT `fk_member_chatroom` FOREIGN KEY (`chatroom_id`) REFERENCES `chatrooms` (`chatroom_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chatroom_members`
+--
+
+LOCK TABLES `chatroom_members` WRITE;
+/*!40000 ALTER TABLE `chatroom_members` DISABLE KEYS */;
+INSERT INTO `chatroom_members` VALUES (1,'17329720396','2025-07-10 06:01:58',NULL,0),(2,'17329720396','2025-07-10 06:04:01',NULL,0),(3,'17896541128','2025-07-10 08:30:20',NULL,0),(4,'17329720396','2025-07-10 12:26:33',NULL,1),(4,'17896541128','2025-07-10 12:25:49',NULL,1);
+/*!40000 ALTER TABLE `chatroom_members` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-10 21:08:58
